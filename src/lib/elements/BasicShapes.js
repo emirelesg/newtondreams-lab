9b6b90eb4223994e623439fa10d14b7a49f52d50
Enrique Mireles <enrique.mireles@udem.edu>
2020-06-20 14:40:31 -0500

    functional simulation

---
+++ src/lib/elements/BasicShapes.js
@@ -0,0 +1,223 @@
+import {
+  Object3D,
+  Mesh,
+  MeshPhongMaterial,
+  BoxBufferGeometry,
+  CylinderBufferGeometry,
+  SphereBufferGeometry,
+  OctahedronBufferGeometry,
+  Raycaster,
+  Vector2,
+  Vector3,
+  Line,
+  LineBasicMaterial,
+  MeshBasicMaterial,
+  BufferGeometry
+} from 'three';
+import { disposeRecursive } from '@/lib/utils';
+import colors from 'vuetify/lib/util/colors';
+
+export default class BasicShapes extends Object3D {
+  constructor(appRef) {
+    super();
+    this.OPACITY = 0.75;
+    this.shapes = [
+      new Mesh(
+        new OctahedronBufferGeometry(5, 0),
+        new MeshPhongMaterial({
+          color: colors.deepPurple.base,
+          opacity: this.OPACITY,
+          transparent: true
+        })
+      ),
+      new Mesh(
+        new CylinderBufferGeometry(5, 5, 10, 20),
+        new MeshPhongMaterial({
+          color: colors.blue.base,
+          opacity: this.OPACITY,
+          transparent: true
+        })
+      ),
+      new Mesh(
+        new BoxBufferGeometry(5, 5, 5),
+        new MeshPhongMaterial({
+          color: colors.green.base,
+          opacity: this.OPACITY,
+          transparent: true
+        })
+      ),
+      new Mesh(
+        new CylinderBufferGeometry(0, 5, 10, 20),
+        new MeshPhongMaterial({
+          color: colors.orange.base,
+          opacity: this.OPACITY,
+          transparent: true
+        })
+      ),
+      new Mesh(
+        new SphereBufferGeometry(5, 20, 20),
+        new MeshPhongMaterial({
+          color: colors.red.base,
+          opacity: this.OPACITY,
+          transparent: true
+        })
+      )
+    ];
+
+    this.movingPoint = new Mesh(
+      new SphereBufferGeometry(0.5, 10, 10),
+      new MeshBasicMaterial({ color: colors.teal.accent3 })
+    );
+    this.startPoint = new Mesh(
+      new SphereBufferGeometry(0.5, 10, 10),
+      new MeshBasicMaterial({ color: colors.red.accent3 })
+    );
+    this.endPoint = new Mesh(
+      new SphereBufferGeometry(0.5, 10, 10),
+      new MeshBasicMaterial({ color: colors.red.accent3 })
+    );
+    this.startPoint.visible = false;
+    this.endPoint.visible = false;
+
+    this.guide = new Line(
+      new BufferGeometry().setFromPoints([new Vector3(), new Vector3()]),
+      new LineBasicMaterial({ color: colors.red.accent3 })
+    );
+
+    this.raycaster = new Raycaster();
+    this.localPoint = new Vector3();
+    this.closestPoint = new Vector3();
+    this.distance = null;
+    this.mouse = new Vector2();
+    this.isMouseDown = false;
+
+    this.appRef = appRef;
+    this.events = ['mousemove', 'mouseup', 'mousedown'];
+    this.bindListeners();
+
+    this.shapes.forEach((obj, i) => {
+      const { height, radius } = obj.geometry.parameters;
+      obj.position.x = 25 * (i / 2 - 1);
+      obj.position.y = height ? height / 2 : radius;
+      obj.receiveShadow = true;
+      obj.castShadow = true;
+    });
+
+    this.add(
+      ...this.shapes,
+      this.startPoint,
+      this.endPoint,
+      this.movingPoint,
+      this.guide
+    );
+  }
+  bindListeners() {
+    this.events.forEach(e => {
+      this[e] = this[e].bind(this);
+      this.appRef.renderer.domElement.addEventListener(e, this[e]);
+    });
+  }
+  destroy() {
+    this.events.forEach(e => {
+      this.appRef.renderer.domElement.removeEventListener(e, this[e]);
+    });
+    this.appRef = null;
+    disposeRecursive(this);
+  }
+  mousedown() {
+    this.isMouseDown = true;
+  }
+  mouseup() {
+    if (this.movingPoint.visible) {
+      if (
+        (this.startPoint.visible && this.endPoint.visible) ||
+        (!this.startPoint.visible && !this.endPoint.visible)
+      ) {
+        this.guide.visible = false;
+        this.distance = null;
+        this.startPoint.visible = true;
+        this.endPoint.visible = false;
+        this.startPoint.position.copy(this.movingPoint.position);
+      } else {
+        this.guide.visible = true;
+        this.endPoint.visible = true;
+        this.endPoint.position.copy(this.movingPoint.position);
+        this.drawGuide();
+      }
+    }
+    this.isMouseDown = false;
+  }
+  mousemove({ target, clientX, clientY }) {
+    var rect = target.getBoundingClientRect();
+    this.mouse.set(
+      ((clientX - rect.left) / target.width) * 2 - 1,
+      (-(clientY - rect.top) / target.height) * 2 + 1
+    );
+    this.raycaster.setFromCamera(this.mouse, this.appRef.camera);
+    const intersects = this.raycaster.intersectObjects(this.shapes);
+    if (intersects.length > 0 && !this.isMouseDown) {
+      let point = this.getClosestPointToEdge(intersects[0]);
+      this.movingPoint.visible = true;
+      this.movingPoint.position.copy(point);
+    } else {
+      this.movingPoint.visible = false;
+    }
+  }
+  drawGuide() {
+    const pos = this.guide.geometry.attributes.position;
+    pos.setXYZ(
+      0,
+      this.startPoint.position.x,
+      this.startPoint.position.y,
+      this.startPoint.position.z
+    );
+    pos.setXYZ(
+      1,
+      this.endPoint.position.x,
+      this.endPoint.position.y,
+      this.endPoint.position.z
+    );
+    this.distance = this.startPoint.position.distanceTo(this.endPoint.position);
+    pos.needsUpdate = true;
+  }
+  getClosestPointToEdge({ point, object, face }) {
+    const pos = object.geometry.attributes.position;
+
+    this.localPoint.copy(point);
+    object.worldToLocal(this.localPoint);
+
+    const facePoints = [
+      new Vector3().fromBufferAttribute(pos, face.a),
+      new Vector3().fromBufferAttribute(pos, face.b),
+      new Vector3().fromBufferAttribute(pos, face.c)
+    ];
+
+    let minDistanceSq = Infinity;
+    facePoints.forEach(point => {
+      const distSq = point.distanceToSquared(this.localPoint);
+      if (distSq < minDistanceSq) {
+        minDistanceSq = distSq;
+        this.closestPoint.copy(point);
+      }
+    });
+
+    // const faceLines = [
+    //   new Line3(facePoints[0], facePoints[1]),
+    //   new Line3(facePoints[0], facePoints[2]),
+    //   new Line3(facePoints[1], facePoints[2])
+    // ];
+
+    // const closestPointToLine = new Vector3();
+    // faceLines.forEach(line => {
+    //   line.closestPointToPoint(this.localPoint, true, closestPointToLine);
+    //   const distSq = closestPointToLine.distanceToSquared(this.localPoint);
+    //   if (distSq < minDistanceSq) {
+    //     minDistanceSq = distSq;
+    //     this.closestPoint.copy(closestPointToLine);
+    //   }
+    // });
+
+    object.localToWorld(this.closestPoint);
+    return this.closestPoint.clone();
+  }
+}
--- src/simulations/mediciones.vue
+++ src/simulations/mediciones.vue
@@ -1,23 +1,38 @@
 <template>
-  <div ref="parent" id="scene"></div>
+  <div ref="parent" class="full">
+    <sim-header
+      title="Mediciones"
+      description="Mide los objectos haciendo clic sobre dos vertices."
+    >
+      <div class="primary--text font-weight-bold">
+        {{ getDistance() }}
+      </div>
+    </sim-header>
+  </div>
 </template>
 
 <script>
+import SimHeader from '@/components/SimHeader.vue';
 import { state, mutations } from '@/store/index';
+import BasicShapes from '@/lib/elements/BasicShapes';
+import { round, gaussianRandom } from '@/lib/utils';
 import App from '@/lib/index';
-
-const signals = [];
 
 export default {
   name: 'Mediciones',
-  components: {},
+  components: {
+    SimHeader
+  },
   data: () => ({
-    app: null
+    app: null,
+    model: null
   }),
   watch: {},
   methods: {
     setup() {
       this.app = new App(this.$refs.parent, {});
+      this.model = new BasicShapes(this.app);
+      this.app.scene.add(this.model);
       this.app.renderer.setAnimationLoop(this.draw.bind(this));
     },
     reset() {},
@@ -25,30 +40,33 @@ export default {
       this.app.controls.update();
       this.app.renderer.render(this.app.scene, this.app.camera);
     },
-    simulate() {}
+    simulate() {},
+    getDistance() {
+      if (this.model && this.model.distance) {
+        return `${round(
+          this.model.distance + gaussianRandom(-0.2, 0.2),
+          2
+        )} cm`;
+      }
+      return `-`;
+    }
   },
   mounted() {
     this.$nextTick(() => {
+      // mutations.resetSim();
+      mutations.setEnabledControls([]);
+      mutations.setEnabledWindows(['data']);
       this.setup();
-      mutations.setSimSignals(signals);
-      mutations.resetSim();
+      this.reset();
     });
     state.bus.$on('reset', this.reset);
   },
   beforeDestroy() {
+    this.model.destroy();
     this.app.destroy();
     this.app = null;
+    this.model = null;
     state.bus.$off('reset', this.reset);
   }
 };
 </script>
-
-<style scoped>
-#scene {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
-</style>
