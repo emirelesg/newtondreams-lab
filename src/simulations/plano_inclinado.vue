18f353842a4ebd196ca33fbedd9b0ddf1391d500
Enrique Mireles <enrique.mireles@udem.edu>
2020-06-20 14:41:22 -0500

    restructure

--- src/simulations/plano_inclinado.vue
+++ src/simulations/plano_inclinado.vue
@@ -1,12 +1,19 @@
 <template>
-  <div ref="parent" id="scene"></div>
+  <div ref="parent" class="full">
+    <sim-header
+      title="Plano Inclinado"
+      description="Analiza el movimiento en un vehiculo en un plano inclinado."
+    >
+    </sim-header>
+  </div>
 </template>
 
 <script>
+import SimHeader from '@/components/SimHeader.vue';
 import { state, mutations } from '@/store/index';
 import App from '@/lib/index';
-import Rail from '@/lib/elements/RailSystem';
-import { round } from '@/lib/utils';
+import RailSystem from '@/lib/elements/RailSystem';
+import { round, guassianNoiseIf } from '@/lib/utils';
 
 const signals = [
   {
@@ -18,46 +25,69 @@ const signals = [
   {
     name: 'Posición',
     var: 'x',
-    units: 'cm'
+    units: 'm'
   },
   {
     name: 'Velocidad',
     var: 'v',
-    units: 'cm/s'
+    units: 'm/s'
   },
   {
     name: 'Aceleración',
     var: 'a',
-    units: 'cm/s^2'
+    units: 'm/s^2'
   }
 ];
 
+const controls = {
+  angle: {
+    type: 'slider',
+    label: 'Inclinación',
+    min: 5,
+    max: 20,
+    step: 1,
+    value: 10,
+    suffix: '°'
+  }
+  // mass: {
+  //   type: 'slider',
+  //   label: 'Masa',
+  //   min: 0.1,
+  //   max: 1,
+  //   step: 0.1,
+  //   value: 0.1,
+  //   suffix: 'kg'
+  // }
+};
+
 export default {
   name: 'PlanoInclinado',
-  components: {},
+  components: {
+    SimHeader
+  },
   data: () => ({
     app: null,
     box: null,
     animation: [],
     i: 0
   }),
-  watch: {},
   methods: {
     setup() {
       this.app = new App(this.$refs.parent, {});
-      this.model = new Rail();
+      this.model = new RailSystem();
       this.app.scene.add(this.model);
       this.app.renderer.setAnimationLoop(this.draw.bind(this));
     },
     reset() {
+      this.model.setInclination(-this.controls.angle.value);
       this.i = 0;
-      this.animation = this.simulate(1 / 60, false);
+      this.animation = this.simulate(1 / 50, false);
       mutations.setSimData(this.simulate(state.sim.sampleTime, true));
     },
     draw() {
       const { t, x } = this.animation[this.i];
       if (this.model.car) {
-        this.model.car.position.x = -37 + x;
+        this.model.car.position.x = -37 + x * 100;
       }
       if (state.sim.isRunning) {
         mutations.updateSimDisplayLimit(t);
@@ -71,21 +101,25 @@ export default {
       this.app.renderer.render(this.app.scene, this.app.camera);
     },
     simulate(dt, noise) {
-      const v0 = 100;
-      const x0 = 2;
-      let x = x0;
-      let v = v0;
+      const theta = (this.controls.angle.value * Math.PI) / 180;
+      const sinx = Math.sin(theta);
+      const cosx = Math.cos(theta);
+      const g = 9.81;
+      const uk = 0.1;
+      let x = 0.02;
+      const a = g * (sinx - uk * cosx);
+      let v = 0;
       let t = 0;
       let signals = [];
-      while (x <= 85) {
-        let noiseX = noise ? 0.1 * (Math.random() * 6 - 3) : 0;
-        let noiseV = noise ? 0.1 * (Math.random() * 6 - 3) : 0;
+      while (x <= 0.85) {
         signals.push({
           t: round(t, 2),
-          x: round(x + noiseX, 2),
-          v: round(v + noiseV, 2),
-          a: 0
+          x: round(x + guassianNoiseIf(noise, 0.003), 4),
+          v: round(v + guassianNoiseIf(noise, 0.003), 4),
+          a: round(a + guassianNoiseIf(noise, 0.003), 4)
         });
+        if (a <= 0) break;
+        v += a * dt;
         x += v * dt;
         t += dt;
       }
@@ -93,12 +127,18 @@ export default {
     }
   },
   mounted() {
+    state.bus.$on('reset', this.reset);
     this.$nextTick(() => {
-      this.setup();
+      mutations.setEnabledControls(['start']);
+      mutations.setEnabledWindows(['graph', 'data', 'settings']);
       mutations.setSimSignals(signals);
+      mutations.setSimControls(controls);
+      this.setup();
       mutations.resetSim();
     });
-    state.bus.$on('reset', this.reset);
+  },
+  computed: {
+    controls: () => state.sim.controls
   },
   beforeDestroy() {
     this.model.destroy();
@@ -109,13 +149,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-#scene {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
-</style>
