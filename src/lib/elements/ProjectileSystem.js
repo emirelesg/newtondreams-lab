16bd66916d35db852a8484a677376fc7d793af69
Enrique Mireles <enrique.mireles@udem.edu>
2020-07-04 13:16:57 -0500

    Draw a ruler every 5 cm

--- src/lib/elements/ProjectileSystem.js
+++ src/lib/elements/ProjectileSystem.js
@@ -7,8 +7,11 @@ import {
   BufferGeometry,
   LineBasicMaterial,
   Line,
-  BufferAttribute
+  BufferAttribute,
+  Group,
+  PlaneBufferGeometry
 } from 'three';
+
 import colors from 'vuetify/lib/util/colors';
 
 export default class ProjectileSystem extends BaseSystem {
@@ -16,6 +19,8 @@ export default class ProjectileSystem extends BaseSystem {
     super(['cannon/cannon.gltf', 'cannon/cannon-body.gltf']);
     this.cannon = null;
     this.cannonBody = null;
+
+    // Path.s
     this.path = new Line(
       new BufferGeometry(),
       new LineBasicMaterial({ color: colors.pink.accent4 })
@@ -24,13 +29,55 @@ export default class ProjectileSystem extends BaseSystem {
       'position',
       new BufferAttribute(new Float32Array(3 * 75), 3)
     );
+
+    // Projectile.
     this.projectile = new Mesh(
       new SphereBufferGeometry(1.5, 15, 15),
       new MeshPhongMaterial({
         color: new Color('#aaaaaa')
       })
     );
+
+    // Ruler.
+    this.ruler = new Group();
+    this.rulerMaterial = new MeshPhongMaterial({
+      color: new Color('#777777')
+    });
+    this.ruler.position.y = -1.49;
+    this.ruler.rotation.x = -Math.PI / 2;
+    this.add(this.ruler);
+    this.loadFont().then(this.drawRuler.bind(this));
+
     this.position.set(-35, -2.5, 0);
+  }
+  drawRuler() {
+    for (let i = 2; i < 15; i += 1) {
+      // Position where a tick will be drawn.
+      const x = i * 5;
+
+      // Create a text geometry and compute its bounding box to center it later.
+      const textGeo = this.getTextGeo(`${x} cm`, { size: 0.9, height: 0 });
+
+      // Build the meshes for the tick and geomtery.
+      const tick = new Mesh(
+        new PlaneBufferGeometry(0.2, 5),
+        this.rulerMaterial
+      );
+      const text = new Mesh(
+        new BufferGeometry().fromGeometry(textGeo),
+        this.rulerMaterial
+      );
+
+      // Position text below tick. Also space ticks using the previously calculated
+      // position.
+      var textCenterOffset =
+        -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+      tick.position.x = x - 0.1;
+      text.position.x = tick.position.x + textCenterOffset;
+      text.position.y = -4;
+
+      this.ruler.add(tick, text);
+    }
   }
   setPath(data) {
     data.forEach(({ x, y }, i) => {
@@ -63,6 +110,9 @@ export default class ProjectileSystem extends BaseSystem {
   }
   destroy() {
     this.dispose();
+    this.rulerMaterial.dispose();
+    this.rulerMaterial = null;
+    this.ruler = null;
     this.cannon = null;
     this.cannonBody = null;
     this.projectile = null;
