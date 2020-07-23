238e79c8ab3e9adf86d71d7c1fb6004e63e60a90
Enrique Mireles <enrique.mireles@udem.edu>
2020-07-23 17:03:13 -0500

    able to modify proojectile radius

--- src/lib/elements/FreeFallSystem.js
+++ src/lib/elements/FreeFallSystem.js
@@ -21,6 +21,10 @@ export default class FreeFallSystem extends Base {
     // 0.3 cm are the thickness of the bottom plate.
     this.position.y = -4 + 0.3;
   }
+  setProjectileRadius(r) {
+    this.projectile.geometry.dispose();
+    this.projectile.geometry = new SphereBufferGeometry(r, 15, 15);
+  }
   updateHeight(h) {
     const scaledH = h * 10;
     const elements = [
@@ -44,8 +48,11 @@ export default class FreeFallSystem extends Base {
       obj => obj.name === 'b_gripper_arm_001_001'
     );
     let percent = constain(k, 0, 1);
-    girpperArmLeft.position.x = -5 * percent;
-    girpperArmRight.position.x = 5 * percent;
+    // This adjusts jaws for projectiles whose radius is not 1.5. Also
+    // scale by 10 since the model is reduced by this same factor.
+    let offset = (1.5 - this.projectile.geometry.parameters.radius) * 10;
+    girpperArmLeft.position.x = -5 * percent + offset;
+    girpperArmRight.position.x = 5 * percent - offset;
   }
   onLoad([gripper]) {
     this.gripper = gripper;
